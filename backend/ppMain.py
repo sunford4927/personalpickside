9ce4f3cbdeaf891b03ce9dd8f06ba0d41d8e63c1
jw <jw39@gmail.com>
2024-08-22 12:07:10 +0900

    지원 커밋

--- backend/ppMain.py
+++ backend/ppMain.py
@@ -8,7 +8,7 @@ class ppMainPage(Resource):
     def get(self):
         data = setQuery("""select idx, brand_name, cos_name, cos_img_src, 
                             grade, grade_count, price, vol
-                            from cos_data""")
+                            from result_product""")
         return jsonify(data)
     
 
@@ -21,4 +21,25 @@ class ppCategorySel(Resource):
         print("cateValue : ",cateValue)
         data = setQuery(f'select * from cos_data where cos_name like "%{cateValue}%" limit 9')
         # data = setQuery("""select * from cos_data""")
+        return jsonify(data)
+    
+
+
+# 메인 화면 중 나이대별 추천 데이터 전송 서버
+class ppSuggestAge(Resource):
+
+    def get(self):
+        # ageDic = request.args.to_dict()
+        # ageval = ageDic['age']
+        age = 10
+        oldAge = age + 9
+        data = setQuery(""" SELECT p.*
+                                FROM result_product p
+                                JOIN result_review r ON p.cos_name = r.cos_name
+                                JOIN result_users u ON r.user_nm = u.user_nm
+                                WHERE u.user_age BETWEEN %s AND %s
+                                GROUP BY p.idx, p.brand_name, p.cos_name, p.cos_img_src, p.grade, p.grade_count, p.price, p.vol, p.ranking, p.category
+                                ORDER BY COUNT(*) DESC
+                                LIMIT 6; """, age, oldAge)
+        
         return jsonify(data)
