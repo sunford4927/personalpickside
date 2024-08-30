1b1f94ea15b7b59d0cc242f64e3c81015088fd3e
오세원 <energizer710@naver.com>
2024-08-30 17:05:26 +0900

    세원 08/30 장바구니 디테일 완료 ver0.2

--- src/page/shoppingcart/ShoppingCart.jsx
+++ src/page/shoppingcart/ShoppingCart.jsx
@@ -97,7 +97,7 @@ const ShoppingCart = () => {
 
             <p className='sampleroad_text'>샘플로드 배송상품</p>
 
-            <div className='basket_line flex_col ' style={{alignItems:"center"}}>
+            <div className='flex_col ' style={{alignItems:"center"}}>
                 <input type="checkbox" className='total_check'  onChange={(e)=> totalCheck(e)}/>
                 <div className='totalcheck_text' >전체선택</div>
                 <div className='deletebtn_text' style={{marginRight : "30px", marginLeft : "auto"}}>품절삭제</div>
