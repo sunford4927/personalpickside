014dbf27b867b7e9956fe0ff0479fd6b8323fa0f
김도연 <dy240319@gmail.com>
2024-09-04 09:25:37 +0900

    123

--- backend/recommend/recommend.py
+++ backend/recommend/recommend.py
@@ -8,34 +8,37 @@ import numpy as np
 data = reco_data()
 df_ing_reco, df_ing_effect, df_ing, df_product, df_review, df_user = data.get_data()
 
-# class reco_simple():
-#     def recommend(user_data):
-# 사용할 유저 정보 저장
-user = '더무아'
-user_data = {
-    '나이': df_user[df_user['user_nm']==user]['user_age_group'].values[0],
-    '성별': df_user[df_user['user_nm']==user]['user_sex'].values[0],
-    '피부타입': df_user[df_user['user_nm']==user]['skin_type'].values[0]
-}
+class reco_simple():
+    def recommend(user):
+        print('user : ', user)
+        # 사용할 유저 정보 저장(요청할 때 받아오기)
+        # user = '더무아'
+        user_data = {
+            '나이': df_user[df_user['user_nm']==user]['user_age_group'].values[0],
+            '성별': df_user[df_user['user_nm']==user]['user_sex'].values[0],
+            '피부타입': df_user[df_user['user_nm']==user]['skin_type'].values[0]
+        }
 
-simple = reco_simple.recommend(user_data)
-cosine = reco_cosine.recommend_cosine(user)
+        simple = reco_simple.recommend(user_data)
+        cosine = reco_cosine.recommend_cosine(user)
 
-# 단순 추천 최고점 화장품 중 랜덤 한개
-simple_reco = simple[simple == max(simple)]
-ran_simple = simple_reco.loc[np.random.choice(simple_reco.index, size=1, replace=False)]
+        # 단순 추천 최고점 화장품 중 랜덤 한개
+        simple_reco = simple[simple == max(simple)]
 
-# 협업 필터링 최고점 화장품 중 랜덤 네개
-cos_reco = cosine[cosine == max(cosine)]
-if len(cos_reco) > 4 :
-    temp = np.random.choice(cos_reco.index, size=4, replace = False)
-else:
-    # 4개 미만일 경우 모두 선택
-    temp = cos_reco.index
-ran_cos = cos_reco.loc[temp]
+        # 협업 필터링 최고점 화장품 중 랜덤 네개
+        cos_reco = cosine[cosine == max(cosine)]
+        if len(cos_reco) > 4 :
+            temp = np.random.choice(cos_reco.index, size=4, replace = False)
+        else:
+            # 4개 미만일 경우 모두 선택
+            temp = cos_reco.index
+        ran_cos = cos_reco.loc[temp]
+        ran_simple = simple_reco.loc[np.random.choice(simple_reco.index, size = 6 - len(cos_reco), replace=False)]
 
-final_reco = pd.concat([ran_cos, ran_simple])
-print(final_reco)
 
-# print(simple)
-# print(cosine)
+        final_reco = pd.concat([ran_cos, ran_simple])
+        print(final_reco)
+
+        return final_reco
+        # print(simple)
+        # print(cosine)
