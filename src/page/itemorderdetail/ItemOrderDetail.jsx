15fa1c3537a4a40d2ac21ce2dca929a9b1f71759
오세원 <energizer710@naver.com>
2024-09-09 12:21:10 +0900

    세원 커밋

--- src/page/itemorderdetail/ItemOrderDetail.jsx
+++ src/page/itemorderdetail/ItemOrderDetail.jsx
@@ -15,22 +15,24 @@ const ItemOrderDetail = () => {
         <>
             <PageHeader title={"주문상세"}/>
             <p className='detail_orderKey detail_p'>주문번호 {state[0].payment_key}</p>
-            <p className='detail_p'>결제날짜 {state[0].order_date}</p>
+            <p className='detail_p'>결제 날짜 {state[0].order_date}</p>
         
             {state.map((item, i) =>{
                 return (
                 <div className='detail_container'>                    
                     <div className='flex_col'>
                         <img className='orderdetail_img' src={item.cos_img_src} alt="" />
+                        <div className='detail_text12'>
                         <div className='detail_text'>
                             <p className='orderdetail_brand'>{item.brand_name}</p> 
                             <p className='orderdetail_name'>{item.cos_name}</p> 
-                            <p className='orderdetail_vol'>{item.vol}</p> </div>
+                            <p className='orderdetail_vol'>{item.vol}</p> 
+                        </div>
 <br />
-                            <div className='detail_text2'>
+                        <div className='detail_text2'>
                             <p className='orderdetail_price'>{item.price*getCnt(item.idx_cnt, i)}원</p>  
                             <p className='orderdetail_brand'>{getCnt(item.idx_cnt, i)}개</p> 
-                           
+                            </div>
                         </div>
                     </div>
                     <div className='detail_btn' onClick={()=> nav('/itemOrderReview',{state : item})}>
